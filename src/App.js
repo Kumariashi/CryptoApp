0386b563bc78bf9602c4077b5450602ce15778a9
Ashika <kumariashika16@gmail.com>
2024-05-30 14:28:15 +0530

    Created a real time crypto-market information application using React and Chakra UI, integrating cryptocurrency APIs to offer up-to-date data on prices, market cap, and trading volume

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import CoinDetails from "./components/CoinDetails";
+import Exchanges from "./components/Exchanges";
+import Coins from "./components/Coins";
+
+function App() {
+  return (
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/coins" element={<Coins></Coins>}></Route>
+        <Route path="/exchanges" element={<Exchanges></Exchanges>}></Route>
+        <Route path="/coins/:id" element={<CoinDetails></CoinDetails>}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { HStack } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '@chakra-ui/react'
+
+const Header = () => {
+  return (
+    <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
+        <Button variant={"unstyled"} color={"white"}>
+            <Link to={'/'}>Home</Link>
+        </Button>
+        <Button variant={"unstyled"} color={"white"}>
+            <Link to={'/exchanges'}>Exchanges</Link>
+        </Button>
+        <Button variant={"unstyled"} color={"white"}>
+            <Link to={'/coins'}>Coins</Link>
+        </Button>
+
+    </HStack>
+  )
+}
+
+export default Header
